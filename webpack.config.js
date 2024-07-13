d76c3c7aba907d8a2f3c70905b5aa85006f5539f
truong <ntruong@switch.tv>
2024-07-13 22:00:26 +0700

    finish base configuration

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    port: 3000,
+    allowedHosts: "all",
+    historyApiFallback: true,
+  },
+};
