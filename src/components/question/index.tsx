6e8bbbd9778f771e8438898f99d59c94d9cf15df
truong <ntruong@switch.tv>
2024-07-16 10:21:10 +0700

    install dayjs

--- src/components/question/index.tsx
+++ src/components/question/index.tsx
@@ -1,5 +1,20 @@
+import dayjs from "dayjs";
+import { Button } from "../button";
+
 import "./question.style.scss";
 
 export const Question = () => {
-  return <div className="question-container">Question thumbnail</div>;
+  return (
+    <div className="question-container">
+      <div className="description">
+        <h4>sarahedo</h4>
+        <p className="time-stamp">
+          {dayjs(1468479767190).format("hh:mm A | MM/DD/YYYY")}
+        </p>
+      </div>
+      <div className="action">
+        <Button>View</Button>
+      </div>
+    </div>
+  );
 };
